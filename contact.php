41fa69622734d3b9c91f6b287a86fa2e37a5acc0
Darby <dssumner@nic.edu>
2024-01-22 15:33:00 -0800

    Separating components

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+$page_title = "Contact";
+?>
+
+<body>
+
+    <?php include('./components/nav.php');
+    ?>
+
+    <h1>Contact Page</h1>
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+<?php include('./components/head.php');
+?>
+   <h1>Home Page</h1> 
+</body>
+</html>
