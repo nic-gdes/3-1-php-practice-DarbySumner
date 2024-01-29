ec8148d760eb02b4b9906baab41e7f903341fee9
Darby <dssumner@nic.edu>
2024-01-28 20:11:29 -0800

    Logo links to home

---
+++ components/nav.php
@@ -0,0 +1,7 @@
+<div class="nav-menu">
+    <nav>
+        <a href="index.php">
+            <img src="../assets/Images/Design_by_Darbs_Logo copy.png" width="auto" height="150px" alt="Design by Darbs Logo">
+        </a>
+    </nav>
+</div>
--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <body>
-<?php include('./components/head.php')
+<?php include('./components/head.php');
+      include('./components/nav.php');
 ?>
-   <h1>Home Page</h1>
-   
-   <header>
-      <img src="../assets/Images/Design_by_Darbs_Logo copy.png" alt="">
-   </header>
+ <header>
+      
+</header>
+
+   <div class="grid-container">
+
+   </div>
+ 
+
 </body>
 </html>
