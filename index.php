1acc8935816853438324876c378b4086b245b87f
Darby <dssumner@nic.edu>
2024-01-28 19:27:18 -0800

    adding logo

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <?php include('./components/head.php')
 ?>
    <h1>Home Page</h1>
-
+   
    <header>
-      <img src="" alt="">
+      <img src="../assets/Images/Design_by_Darbs_Logo copy.png" alt="">
    </header>
 </body>
 </html>
