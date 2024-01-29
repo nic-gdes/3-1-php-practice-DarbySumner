40385705a59e1f794b565c1a8d537846b18c3649
Darby <dssumner@nic.edu>
2024-01-28 19:19:27 -0800

    Directory Structure Organization

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <body>
 <?php include('./components/head.php')
 ?>
-   <h1>Home Page</h1> 
+   <h1>Home Page</h1>
+
+   <header>
+      <img src="" alt="">
+   </header>
 </body>
 </html>
