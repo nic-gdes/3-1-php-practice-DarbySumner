f27fdca5752e49efdcc7d9973cf6acc7f923781a
Darby <dssumner@nic.edu>
2024-01-30 15:26:42 -0800

    Adding copyright and adjusting the styling, trying to fix comments not working.

--- components/footer.php
+++ components/footer.php
@@ -1,11 +1,16 @@
+
 <div class="footer">
 
-<?php echo date("Y") ?>
+//Prints the copyright
 
-<?php
+<p>&copy; <?php echo date("Y"); ?>
+
+NIC GDES 261
+
+<?php 
 
 // Prints the day, date, month, year, time, AM or PM
 echo date("l jS \of F Y h:i:s A");
 
-?> 
+?></p>
 </div>
