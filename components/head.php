86dc53e21ebc35b86ae476a650de8bd2dac16775
Darby <dssumner@nic.edu>
2024-01-30 18:35:39 -0800

    Making grid layout for favorite things and adding content and styling it

--- components/head.php
+++ components/head.php
@@ -4,6 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Design by Darbs</title>
     <link rel="stylesheet" href="https://use.typekit.net/ttn1uda.css">
-    <link rel="stylesheet" href="./assets/CSS/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
 
 </head>
--- index.php
+++ index.php
@@ -3,21 +3,36 @@
 <body>
 <?php include('./components/head.php');
       include('./components/nav.php');
-      include('./components/footer.php');
+     
 ?>
       <main>
-            <div class="grid-container">
+           
                   <div><h1>My Favorite Things</h1></div>
-                  <div><h1>Band:</h1></div>
-                  <div><h1>Artist:</h1></div>
-                  <div><h1>Designer:</h1></div>
-                  <div> <h1>Show:</h1></div>
-                  <div> <h1>Art:</h1></div>
-                  <div><h1>Movie:</h1></div>
+
+                  <div class="grid-container">
+
+                  <div><h1>Band: Twenty One Pilots</h1>
+                        <img src="assets/Images/21Pilots.jpg" alt="21Pilots Band" height="400px" class="image1">
+                  </div>
+                  <div><h1>Artist: Imad Awan</h1>
+                        <img src="assets/Images/ImadAwan.jpg" alt="Imad Awan Concept Artist" height="400px" class="image2">
+                  </div>
+                  <div><h1>Jesse Nyberg:</h1>
+                        <img src="assets/Images/JesseNyberg.jpg" alt="Jesse Nyberg Graphic Designer" height="400px" class="image3">
+                  </div>
+                  <div> <h1>Show: The Ofiice</h1>
+                        <img src="assets/Images/TheOffice.jpg" alt="The Office Show" height="300px" class="image4">
+                  </div>
+                  <div> <h1>Book: As Old As Time</h1>
+                        <img src="assets/Images/AsOldAsTime.png" alt="As Old As Time Book" height="400px" class="image5">
+                  </div>
+                  <div><h1>Movie: Ocean's Eleven'</h1>
+                        <img src="assets/Images/OceansEleven.jpg" alt="Ocean's Eleven Movie" height="400px" class="image6">
+                  </div>
             </div>
       </main>
 
- 
+     <?php include('./components/footer.php'); ?>
 
 </body>
 </html>
